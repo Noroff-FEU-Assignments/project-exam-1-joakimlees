9424fdfb313f7531922acf0a99dd033cb2dbe1bc
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-05-17 20:33:51 +0200

    started media js

--- js/index.js
+++ js/index.js
@@ -6,18 +6,39 @@ const url = "https://joakimlees.no/project-exam-1/wp-json/wp/v2/blog-post?acf_fo
 const apiResult = await getRequest(url);
 
 console.log(apiResult);
+console.log("-----------------------------------------------");
 
-console.log("-------------------------");
+const mediaTest = matchMedia("( min-width: 1000px)");
+/*
+mediaMobil.addEventListener("change", function () {
+  rows = 1;
+  displayCards(apiResult, cardWrapper, rows, count);
+});
+*/
+
+mediaTest.addEventListener("change", function () {
+  rows = 1;
+  displayCards(apiResult, cardWrapper, rows, count);
+});
+
+console.log("-----------------------------------------------");
+
+let count = 1;
+let rows = 3;
+
+if (mediaTest.matches) {
+  rows = 3;
+} else {
+  rows = 1;
+}
 
 const cardWrapper = document.querySelector(".card-wrapper");
 const rightBtn = document.querySelector(".arrow-right");
 const leftBtn = document.querySelector(".arrow-left");
 
-let count = 1;
-let rows = 3;
+function displayCards(cards, container, rows, page) {
+  container.innerHTML = "";
 
-function displayCards(cards, rows, page) {
-  console.clear();
   page--;
 
   let start = rows * page;
@@ -31,32 +52,48 @@ function displayCards(cards, rows, page) {
 
   for (let i = 0; i < currentCards.length; i++) {
     let card = currentCards[i];
+
+    container.innerHTML += `
+    <a class="blogpost-card" href="/html/posts.html?id=${card.id}">
+                <div class="card-image">
+                  <img src="${card.acf.headerimg}" alt="${card.title.rendered} image" />
+                </div>
+                <div class="card-text-wrapper">
+                  <h4>${card.title.rendered}</h4>
+                  <p class="read-card">Read post</p>
+                  <p class="card-date">${card.date}</p>
+                </div>
+              </a>
+
+    
+    
+    `;
   }
 }
 
-displayCards(apiResult, rows, count);
+displayCards(apiResult, cardWrapper, rows, count);
 
 rightBtn.addEventListener("click", function pageRight() {
-  let test = count;
+  let pageInc = count;
   if (count >= apiResult.length / rows) {
-    test = apiResult.length / rows;
+    pageInc = apiResult.length / rows;
   } else {
-    test = count += 1;
+    pageInc = count += 1;
   }
 
-  displayCards(apiResult, rows, test);
+  displayCards(apiResult, cardWrapper, rows, pageInc);
 });
 
 leftBtn.addEventListener("click", function pageLeft() {
-  let test2 = count;
+  let pageDec = count;
 
   if (count < 2) {
-    test2 = 1;
+    pageDec = 1;
   } else {
-    test2 = count -= 1;
+    pageDec = count -= 1;
   }
 
-  displayCards(apiResult, rows, test2);
+  displayCards(apiResult, cardWrapper, rows, pageDec);
 });
 
 /*
