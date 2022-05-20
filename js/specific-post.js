74332fe7acf7cd0c664730c769277a0a93f2d1be
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-05-20 15:15:22 +0200

    made changes

--- js/specific-post.js
+++ js/specific-post.js
@@ -6,11 +6,11 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const postId = params.get("id");
 
-const specificPostUrl = "https://joakimlees.no/project-exam-1/wp-json/wp/v2/blog-post/" + postId + "?acf_format=standard";
+const PostUrl = "https://joakimlees.no/project-exam-1/wp-json/wp/v2/blog-post/" + postId + "?acf_format=standard";
 
-const specificPost = await getRequest(specificPostUrl);
+const specificPostUrl = await getRequest(PostUrl);
 console.log("---------------");
-console.log(specificPost);
+console.log(specificPostUrl);
 console.log("------------");
 
 const headingPostsImage = document.querySelector(".header-image-posts");
@@ -19,83 +19,83 @@ const postContainer = document.querySelector(".blog-post-container");
 displayHtml(specificPostUrl, headingPostsImage, postHeadingHtml);
 
 function postHeadingHtml() {
-  headingPostsImage.innerHTML += ` <img class="post-header-img" src="${specificPost.acf.img1_header}" alt="" /> 
+  headingPostsImage.innerHTML += ` <img class="post-header-img" src="${specificPostUrl.acf.img1_header}" alt="" /> 
   `;
 }
 
-function testHtml() {
-  const date = specificPost.date.slice(0, -9);
-  const updated = specificPost.modified.slice(0, -9);
+function makePostHtml(postObject, container) {
+  const date = postObject.date.slice(0, -9);
+  const updated = postObject.modified.slice(0, -9);
 
-  postContainer.innerHTML = `
-    <h1 class="post-heading">${specificPost.title.rendered}</h1>
-    <p class="post-date">By: ${specificPost.acf.author} | Published: ${date} | Last updated: ${updated}</p>
-    <p class="post-para post-intro-para">${specificPost.acf.intro_para}</p>
-    <a class="post-link post-link-intro" href="${specificPost.acf.link_intro}">${specificPost.acf.link_text_intro}</a>
+  container.innerHTML = `
+    <h1 class="post-heading">${postObject.title.rendered}</h1>
+    <p class="post-date">By: ${postObject.acf.author} | Published: ${date} | Last updated: ${updated}</p>
+    <p class="post-para post-intro-para">${postObject.acf.intro_para}</p>
+    <a class="post-link post-link-intro" href="${postObject.acf.link_intro}">${postObject.acf.link_text_intro}</a>
     <div class="blog-post-img-wrapper post-img-intro">
-      <img class="blog-post-img" src="${specificPost.acf.img_intro}" alt="" />
+      <img class="blog-post-img" src="${postObject.acf.img_intro}" alt="" />
     </div>
-    <h2 class="post-subheading post-subheading1">${specificPost.acf.subheading1}</h2>
-    <p class="post-para post-para1">${specificPost.acf.para1}</p>
-    <a class="post-link post-link1" href="${specificPost.acf.link1}">${specificPost.acf.link_text1}</a>
+    <h2 class="post-subheading post-subheading1">${postObject.acf.subheading1}</h2>
+    <p class="post-para post-para1">${postObject.acf.para1}</p>
+    <a class="post-link post-link1" href="${postObject.acf.link1}">${postObject.acf.link_text1}</a>
     <div class="blog-post-img-wrapper post-img1">
-      <img class="blog-post-img" src="${specificPost.acf.img1}" alt="" />
+      <img class="blog-post-img" src="${postObject.acf.img1}" alt="" />
     </div>
-    <h2 class="post-subheading post-subheading2">${specificPost.acf.subheading2}</h2>
-    <p class="post-para post-para2">${specificPost.acf.para2}</p>
-    <a class="post-link post-link2" href="${specificPost.acf.link2}">${specificPost.acf.link_text2}</a>
+    <h2 class="post-subheading post-subheading2">${postObject.acf.subheading2}</h2>
+    <p class="post-para post-para2">${postObject.acf.para2}</p>
+    <a class="post-link post-link2" href="${postObject.acf.link2}">${postObject.acf.link_text2}</a>
     <div class="blog-post-img-wrapper post-img2">
-      <img class="blog-post-img" src="${specificPost.acf.img2}" alt="" />
+      <img class="blog-post-img" src="${postObject.acf.img2}" alt="" />
     </div>
-    <h2 class="post-subheading post-subheading3">${specificPost.acf.subheading3}</h2>
-    <p class="post-para post-para3">${specificPost.acf.para3}</p>
-    <a class="post-link post-link3"  href="${specificPost.acf.link3}">${specificPost.acf.link_text3}</a>
+    <h2 class="post-subheading post-subheading3">${postObject.acf.subheading3}</h2>
+    <p class="post-para post-para3">${postObject.acf.para3}</p>
+    <a class="post-link post-link3"  href="${postObject.acf.link3}">${postObject.acf.link_text3}</a>
     <div class="blog-post-img-wrapper post-img3">
-      <img class="blog-post-img" src="${specificPost.acf.img3}" alt="" />
+      <img class="blog-post-img" src="${postObject.acf.img3}" alt="" />
     </div>
-    <h2 class="post-subheading post-subheading4">${specificPost.acf.subheading4}</h2>
-    <p class="post-para post-para4">${specificPost.acf.para4}</p>
-    <a class="post-link post-link4"  href="${specificPost.acf.link4}">${specificPost.acf.link_text4}</a>
+    <h2 class="post-subheading post-subheading4">${postObject.acf.subheading4}</h2>
+    <p class="post-para post-para4">${postObject.acf.para4}</p>
+    <a class="post-link post-link4"  href="${postObject.acf.link4}">${postObject.acf.link_text4}</a>
     <div class="blog-post-img-wrapper post-img4">
-      <img class="blog-post-img" src="${specificPost.acf.img4}" alt="" />
+      <img class="blog-post-img" src="${postObject.acf.img4}" alt="" />
     </div>
-    <h2 class="post-subheading post-subheading5">${specificPost.acf.subheading5}</h2>
-    <p class="post-para post-para5">${specificPost.acf.para5}</p>
-    <a class="post-link post-link5"  href="${specificPost.acf.link5}">${specificPost.acf.link_text5}</a>
+    <h2 class="post-subheading post-subheading5">${postObject.acf.subheading5}</h2>
+    <p class="post-para post-para5">${postObject.acf.para5}</p>
+    <a class="post-link post-link5"  href="${postObject.acf.link5}">${postObject.acf.link_text5}</a>
     <div class="blog-post-img-wrapper post-img5">
-      <img class="blog-post-img" src="${specificPost.acf.img5}" alt="" />
+      <img class="blog-post-img" src="${postObject.acf.img5}" alt="" />
     </div>
   
   `;
 }
 
-displayHtml(specificPostUrl, postContainer, testHtml);
+displayHtml(specificPostUrl, postContainer, makePostHtml);
 
-hideIfNotProperty(specificPost.acf.img1_header, ".post-header-img");
+hideIfNotProperty(specificPostUrl.acf.img1_header, ".post-header-img");
 
-hideIfNotProperty(specificPost.acf.subheading1, ".post-subheading1");
-hideIfNotProperty(specificPost.acf.subheading2, ".post-subheading2");
-hideIfNotProperty(specificPost.acf.subheading3, ".post-subheading3");
-hideIfNotProperty(specificPost.acf.subheading4, ".post-subheading4");
-hideIfNotProperty(specificPost.acf.subheading5, ".post-subheading5");
+hideIfNotProperty(specificPostUrl.acf.subheading1, ".post-subheading1");
+hideIfNotProperty(specificPostUrl.acf.subheading2, ".post-subheading2");
+hideIfNotProperty(specificPostUrl.acf.subheading3, ".post-subheading3");
+hideIfNotProperty(specificPostUrl.acf.subheading4, ".post-subheading4");
+hideIfNotProperty(specificPostUrl.acf.subheading5, ".post-subheading5");
 
-hideIfNotProperty(specificPost.acf.intro_para, ".post-intro-para");
-hideIfNotProperty(specificPost.acf.para1, ".post-para1");
-hideIfNotProperty(specificPost.acf.para2, ".post-para2");
-hideIfNotProperty(specificPost.acf.para3, ".post-para3");
-hideIfNotProperty(specificPost.acf.para4, ".post-para4");
-hideIfNotProperty(specificPost.acf.para5, ".post-para5");
+hideIfNotProperty(specificPostUrl.acf.intro_para, ".post-intro-para");
+hideIfNotProperty(specificPostUrl.acf.para1, ".post-para1");
+hideIfNotProperty(specificPostUrl.acf.para2, ".post-para2");
+hideIfNotProperty(specificPostUrl.acf.para3, ".post-para3");
+hideIfNotProperty(specificPostUrl.acf.para4, ".post-para4");
+hideIfNotProperty(specificPostUrl.acf.para5, ".post-para5");
 
-hideIfNotProperty(specificPost.acf.link_intro, ".post-link-intro");
-hideIfNotProperty(specificPost.acf.link1, ".post-link1");
-hideIfNotProperty(specificPost.acf.link2, ".post-link2");
-hideIfNotProperty(specificPost.acf.link3, ".post-link3");
-hideIfNotProperty(specificPost.acf.link4, ".post-link4");
-hideIfNotProperty(specificPost.acf.link5, ".post-link5");
+hideIfNotProperty(specificPostUrl.acf.link_intro, ".post-link-intro");
+hideIfNotProperty(specificPostUrl.acf.link1, ".post-link1");
+hideIfNotProperty(specificPostUrl.acf.link2, ".post-link2");
+hideIfNotProperty(specificPostUrl.acf.link3, ".post-link3");
+hideIfNotProperty(specificPostUrl.acf.link4, ".post-link4");
+hideIfNotProperty(specificPostUrl.acf.link5, ".post-link5");
 
-hideIfNotProperty(specificPost.acf.img_intro, ".post-img-intro");
-hideIfNotProperty(specificPost.acf.img1, ".post-img1");
-hideIfNotProperty(specificPost.acf.img2, ".post-img2");
-hideIfNotProperty(specificPost.acf.img3, ".post-img3");
-hideIfNotProperty(specificPost.acf.img4, ".post-img4");
-hideIfNotProperty(specificPost.acf.img5, ".post-img5");
+hideIfNotProperty(specificPostUrl.acf.img_intro, ".post-img-intro");
+hideIfNotProperty(specificPostUrl.acf.img1, ".post-img1");
+hideIfNotProperty(specificPostUrl.acf.img2, ".post-img2");
+hideIfNotProperty(specificPostUrl.acf.img3, ".post-img3");
+hideIfNotProperty(specificPostUrl.acf.img4, ".post-img4");
+hideIfNotProperty(specificPostUrl.acf.img5, ".post-img5");
