32189d53dc87ad34829a934fc18f6bb3337c4266
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2022-05-26 20:52:28 +0200

    started burger menu

---
+++ js/components/burgerMenu.js
@@ -0,0 +1,6 @@
+const exit = document.querySelector("#exit");
+const burgerMenu = document.querySelector("#hamburger-menu");
+
+exit.addEventListener("click", function () {
+  console.log(burgerMenu);
+});
