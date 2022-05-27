c3f0344a0aab42c30cabde7181dc427c28b73c20
Joakim Lee Sletten <joakimlee_sletten@outlook.com>
2022-05-28 00:09:13 +0200

    moved html files out of html folder and to root

--- js/components/burgerMenu.js
+++ js/components/burgerMenu.js
@@ -1,8 +1,6 @@
 const navCloser = document.querySelector("#nav-closer");
 const burgerMenu = document.querySelector("#hamburger-menu");
 
-console.log(burgerMenu);
-
 navCloser.addEventListener("click", function () {
   burgerMenu.checked = false;
 });
